7361543514076c53f0e55dea8b6aeabcb21fdba2
raven.huang <raven.huang@cloudwise.com>
2026-04-21 23:09:05 +0800

    Feature #Castrel-001 调整镜像

--- scripts/java-service-entrypoint.sh
+++ scripts/java-service-entrypoint.sh
@@ -5,7 +5,6 @@ service_name="${OTEL_SERVICE_NAME:-${CW_APP_NAME:-java-service}}"
 dump_dir="${JAVA_DUMP_DIR:-/service-data/heapdumps}"
 
 mkdir -p "$dump_dir"
-rm -f "$dump_dir"/*.hprof
 
 timestamp="$(date +%Y%m%d-%H%M%S)"
 dump_path="$dump_dir/${service_name}-${timestamp}.hprof"
